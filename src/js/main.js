05144d988502035bd94d92629327f544315050a0
andreluke <andre.l.sales2706@gmail.com>
2025-05-14 20:58:05 -0300

    :bug: fix: Correções pequenas

--- src/js/main.js
+++ src/js/main.js
@@ -10,49 +10,16 @@ $(document).ready(function () {
 
   $(".menu button").click(function () {
     if ($(this).hasClass("sidebar-toggle")) {
-        return;
-      }
+      return;
+    }
 
     var target = $(this).data("target");
     $(".section").removeClass("active");
     $("#" + target).addClass("active");
     $(".sidebar").addClass("hidden");
-  }); 
+  });
 
   $(".project-content").click(function () {
     $(this).find("p").toggleClass("expanded");
   });
-
-  function calcularIdade(dataNascimento) {
-    const hoje = new Date();
-    const nascimento = new Date(dataNascimento);
-    let idade = hoje.getFullYear() - nascimento.getFullYear();
-    const mesAtual = hoje.getMonth();
-    const diaAtual = hoje.getDate();
-
-    if (
-      mesAtual < nascimento.getMonth() ||
-      (mesAtual === nascimento.getMonth() && diaAtual < nascimento.getDate())
-    ) {
-      idade--; // Ainda não fez aniversário este ano
-    }
-    return idade;
-  }
-
-  function calcularSemestre(dataIngresso) {
-    const hoje = new Date();
-    const ingresso = new Date(dataIngresso);
-    let semestre = Math.floor((hoje.getFullYear() - ingresso.getFullYear()) * 2 + (hoje.getMonth() + 1) / 6);
-    
-    if (semestre < 6) {
-        return `${semestre + 1}° semestre`;
-    } else {
-        return "Concluído";
-    }
-}
-
-  // Define a idade no HTML
-
-  $("#conclusao").text(calcularSemestre("2023-02-01"));
-  $("#idade").text(`Idade: ${calcularIdade("2005-06-27")} anos`);
 });
